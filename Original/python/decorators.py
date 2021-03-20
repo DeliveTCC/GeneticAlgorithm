881c1192afbc9b5c1d838e5aac47a91548e19cd1
William Rocha de Oliveira <william.r.o.wro@gmail.com>
2021-03-20 17:57:44 -0300

    Removido o diretório "Original"

--- Original/python/decorators.py
+++
@@ -1,9 +0,0 @@
-def singleton(cls):
-    instances = {}
-
-    def instance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
-
-    return instance
